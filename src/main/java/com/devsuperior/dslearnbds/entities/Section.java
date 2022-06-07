43a08efa696e071b96c4a7ea7845f339654bc5c9
Caio Macedo <caio.macedo-mtm@hotmail.com>
2022-06-07 20:13:06 -0300

    Section

--- src/main/java/com/devsuperior/dslearnbds/entities/Section.java
+++ src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -4,13 +4,11 @@ import com.devsuperior.dslearnbds.entities.enums.ResourceType;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_resource")
-public class Resource implements Serializable {
+@Table(name = "tb_section")
+public class Section implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -20,26 +18,26 @@ public class Resource implements Serializable {
     private String description;
     private Integer position;
     private String imgUri;
-    private ResourceType type;
 
     @ManyToOne
-    @JoinColumn(name = "offer_id")
-    private Offer offer;
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
 
-    @OneToMany(mappedBy = "resource")
-    private List<Section> sections = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "prerequisite_id")
+    private Section prerequisite;
 
-    public Resource() {
+    public Section() {
     }
 
-    public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type, Offer offer) {
+    public Section(Long id, String title, String description, Integer position, String imgUri, Resource resource, Section prerequisite) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.position = position;
         this.imgUri = imgUri;
-        this.type = type;
-        this.offer = offer;
+        this.resource = resource;
+        this.prerequisite = prerequisite;
     }
 
     public Long getId() {
@@ -82,28 +80,28 @@ public class Resource implements Serializable {
         this.imgUri = imgUri;
     }
 
-    public ResourceType getType() {
-        return type;
+    public Resource getResource() {
+        return resource;
     }
 
-    public void setType(ResourceType type) {
-        this.type = type;
+    public void setResource(Resource resource) {
+        this.resource = resource;
     }
 
-    public Offer getOffer() {
-        return offer;
+    public Section getPrerequisite() {
+        return prerequisite;
     }
 
-    public void setOffer(Offer offer) {
-        this.offer = offer;
+    public void setPrerequisite(Section prerequisite) {
+        this.prerequisite = prerequisite;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Resource resource = (Resource) o;
-        return id.equals(resource.id);
+        Section section = (Section) o;
+        return id.equals(section.id);
     }
 
     @Override
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,3 +23,7 @@ INSERT INTO tb_offer (edition,start_Moment,end_Moment,course_id) VALUES ('2.0',T
 INSERT INTO tb_resource (title,description,position,img_Uri,type,offer_id) VALUES ('Trilha HTML','Trilha principal do curso',1,'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png',1,1);
 INSERT INTO tb_resource (title,description,position,img_Uri,type,offer_id) VALUES ('Forum','Tire suas dúvidas',2,'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png',2,1);
 INSERT INTO tb_resource (title,description,position,img_Uri,type,offer_id) VALUES ('Lives','Lives exclusivas para a turma',3,'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png',1,1);
+
+INSERT INTO tb_section (title,description,position,img_Uri,resource_id,prerequisite_id) VALUES ('Capitulo 1','Neste capítulo vamos começar',1,'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png',1,null);
+INSERT INTO tb_section (title,description,position,img_Uri,resource_id,prerequisite_id) VALUES ('Capitulo 2','Neste capítulo vamos continuar',2,'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png',1,1);
+INSERT INTO tb_section (title,description,position,img_Uri,resource_id,prerequisite_id) VALUES ('Capitulo 3','Neste capítulo vamos finalizar',3,'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png',1,2);
