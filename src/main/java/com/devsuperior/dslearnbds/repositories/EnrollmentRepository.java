f9338a52089aafbb183d5cde43d6cd04b9aceb7f
Caio Macedo <caio.macedo-mtm@hotmail.com>
2022-06-08 09:07:07 -0300

    Repositories

---
+++ src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import com.devsuperior.dslearnbds.entities.Course;
+import com.devsuperior.dslearnbds.entities.Enrollment;
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentPK> {
+}
