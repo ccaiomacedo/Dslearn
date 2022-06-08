42df82995377f4628c112a603bae02fadadbfc55
Caio Macedo <caio.macedo-mtm@hotmail.com>
2022-06-07 22:30:04 -0300

    Lessons seed, fix:lessonsDone

--- src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -2,11 +2,10 @@ package com.devsuperior.dslearnbds.entities;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -22,6 +21,9 @@ public class Enrollment {
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessonsDone = new HashSet<>();
 
     public Enrollment() {
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,3 +29,19 @@ INSERT INTO tb_section (title,description,position,img_Uri,resource_id,prerequis
 INSERT INTO tb_section (title,description,position,img_Uri,resource_id,prerequisite_id) VALUES ('Capitulo 3','Neste capítulo vamos finalizar',3,'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png',1,2);
 
 INSERT INTO tb_enrollment (user_id,offer_id,enroll_Moment,refund_Moment,available,only_Update) VALUES (1,1,TIMESTAMP WITH TIME ZONE'2022-10-13T013:00:00Z',null,true,false);
+
+INSERT INTO tb_lesson (title,position,section_id) VALUES ('Aula 1 do capítulo 1',1,1);
+INSERT INTO tb_content (id,text_Content,video_Uri) VALUES (1,'Material de apoio: abc','https://www.youtube.com/watch?v=UlvasL3R1ZU');
+
+INSERT INTO tb_lesson (title,position,section_id) VALUES ('Aula 2 do capítulo 1',2,1);
+INSERT INTO tb_content (id,text_Content,video_Uri) VALUES (2,'','https://www.youtube.com/watch?v=UlvasL3R1ZU');
+
+INSERT INTO tb_lesson (title,position,section_id) VALUES ('Aula 3 do capítulo 1',3,1);
+INSERT INTO tb_content (id,text_Content,video_Uri) VALUES (3,'','https://www.youtube.com/watch?v=UlvasL3R1ZU');
+
+INSERT INTO tb_lesson (title,position,section_id) VALUES ('Tarefa do capítulo 1',4,1);
+INSERT INTO tb_task (id,description,question_Count,approval_Count,weight,due_Date) VALUES (4,'Fazer um trabalho legal',5,4,1.0,TIMESTAMP WITH TIME ZONE'2022-10-25T013:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id,user_id,offer_id) VALUES (1,1,1);
+INSERT INTO tb_lessons_done (lesson_id,user_id,offer_id) VALUES (2,1,1);
+
